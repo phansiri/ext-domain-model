ad178abec03d04cbf1c66d3e9cdc6c1ab987e3bc
lit_phansiri <phansiri@uw.edu>
2016-10-17 21:27:58 -0700

    updated test cases to handle part 2 of the assignment

--- SimpleDomainModelTests/MoneyTests.swift
+++ SimpleDomainModelTests/MoneyTests.swift
@@ -25,6 +25,7 @@ class MoneyTests: XCTestCase {
         let oneUSD = Money(amount: 1, currency: "USD")
         XCTAssert(oneUSD.amount == 1)
         XCTAssert(oneUSD.currency == "USD")
+        XCTAssert(oneUSD.description == "USD1.0")
         
         let tenGBP = Money(amount: 10, currency: "GBP")
         XCTAssert(tenGBP.amount == 10)
@@ -80,8 +81,8 @@ class MoneyTests: XCTestCase {
         let usd = eur.convert("USD")
         XCTAssert(tenUSD.amount == usd.amount)
         XCTAssert(tenUSD.currency == usd.currency)
-        XCTAssert(eur.description == "\(eur.currency)\(eur.amount).0")
-        XCTAssert(usd.description == "\(usd.currency)\(usd.amount).0")
+        XCTAssert(eur.description == "EUR15.0")
+        XCTAssert(usd.description == "USD10.0")
 
     }
     func testUSDtoGBPtoUSD() {
@@ -89,31 +90,64 @@ class MoneyTests: XCTestCase {
         let usd = gbp.convert("USD")
         XCTAssert(tenUSD.amount == usd.amount)
         XCTAssert(tenUSD.currency == usd.currency)
-        XCTAssert(gbp.description == "\(gbp.currency)\(gbp.amount).0")
-        XCTAssert(usd.description == "\(usd.currency)\(usd.amount).0")
+        XCTAssert(gbp.description == "GBP5.0")
+        XCTAssert(usd.description == "USD10.0")
     }
     func testUSDtoCANtoUSD() {
         let can = twelveUSD.convert("CAN")
         let usd = can.convert("USD")
         XCTAssert(twelveUSD.amount == usd.amount)
         XCTAssert(twelveUSD.currency == usd.currency)
-        XCTAssert(can.description == "\(can.currency)\(can.amount).0")
-        XCTAssert(usd.description == "\(usd.currency)\(usd.amount).0")
+        XCTAssert(can.description == "CAN15.0")
+        XCTAssert(usd.description == "USD12.0")
     }
     
     func testAddUSDtoUSD() {
         let total = tenUSD.add(tenUSD)
         XCTAssert(total.amount == 20)
         XCTAssert(total.currency == "USD")
-        XCTAssert(total.description == "\(total.currency)\(total.amount).0")
+        XCTAssert(total.description == "USD20.0")
     }
     
     func testAddUSDtoGBP() {
         let total = tenUSD.add(fiveGBP)
         XCTAssert(total.amount == 10)
         XCTAssert(total.currency == "GBP")
-        XCTAssert(total.description == "\(total.currency)\(total.amount).0")
+        XCTAssert(total.description == "GBP10.0")
 
+    }
+    
+    func testSubtractTwelveUSDfromTenUSD() {
+        let difference = twelveUSD.subtract(tenUSD)
+        XCTAssert(difference.amount == 2)
+    }
+    
+    func testExtensionTwelveUSD() {
+        let usd = 12.USD
+        XCTAssert(usd.amount == 12)
+        XCTAssert(usd.currency == "USD")
+        XCTAssert(usd.description == "USD12.0")
+    }
+    
+    func testExtensionFiftyFiveEUR() {
+        let usd = 55.EUR
+        XCTAssert(usd.amount == 55)
+        XCTAssert(usd.currency == "EUR")
+        XCTAssert(usd.description == "EUR55.0")
+    }
+    
+    func testExtensionTwentyThreeGBP() {
+        let usd = 23.GBP
+        XCTAssert(usd.amount == 23)
+        XCTAssert(usd.currency == "GBP")
+        XCTAssert(usd.description == "GBP23.0")
+    }
+    
+    func testExtensionNintyThousandTwoHundredEightFourYEN() {
+        let usd = 90284.YEN
+        XCTAssert(usd.amount == 90284)
+        XCTAssert(usd.currency == "YEN")
+        XCTAssert(usd.description == "YEN90284.0")
     }
 }
 
